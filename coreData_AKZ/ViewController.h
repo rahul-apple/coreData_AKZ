d1e6df28a4d233ebc18601ad89be66320d9924d5
Rahul R <rahulthazhuthala@gmail.com>
2015-05-09 10:05:36 +0530

    Initial Commit

---
+++ coreData_AKZ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coreData_AKZ
+//
+//  Created by Rahul on 09/05/15.
+//  Copyright (c) 2015 Rahul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ coreData_AKZTests/coreData_AKZTests.m
@@ -0,0 +1,40 @@
+//
+//  coreData_AKZTests.m
+//  coreData_AKZTests
+//
+//  Created by Rahul on 09/05/15.
+//  Copyright (c) 2015 Rahul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface coreData_AKZTests : XCTestCase
+
+@end
+
+@implementation coreData_AKZTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
